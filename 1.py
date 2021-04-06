df72b0b3dbdab31cf0a0f849e15f599e6863b4a9
Petr-creat-pg <81649008+Petr-creat-pg@users.noreply.github.com>
2021-04-06 19:26:40 +0300

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,18 @@
+
+a=10
+b=5
+print(a)
+print(b)
+print('введите числа')
+c=int(input())
+d=int(input())
+print('введите строки')
+e=str(input())
+f=str(input())
+
+print(c)
+print(d)
+print(e)
+print(f)
+
+
